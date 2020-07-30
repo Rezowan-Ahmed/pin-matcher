1015022bb9a3289fb9dd23f91985b7b8f01df723
Rezowan-Ahmed <ahmedrezowan07@gmail.com>
2020-07-30 10:00:50 +0800

    fixed random number

--- main.js
+++ main.js
@@ -1,44 +1,51 @@
-document.getElementById("clickGenerateBtn").addEventListener('click', function(){
-    const randomNumber = Math.floor(1000 + Math.random() * 9999);
+document.getElementById('clickGenerateBtn').addEventListener('click',function(){
+    let randomNumber;
+    for(let i = 0; i < 1000; i++){
+        let random = Math.random() * 9999;
+        randomNumber = Math.floor(random);
+        }
     document.getElementById("generateInputBox").value = randomNumber;
 });
-document.getElementById("pinSubmitBtn").addEventListener('click', function(){
-    const generateInput = document.getElementById("generateInputBox").value;
-    const submitInput = document.getElementById("submitInputBox").value;
-    if( generateInput === submitInput ){
-        const submitDone = document.getElementById("successMessage");
+document.getElementById('pinSubmitBtn').addEventListener('click', function(){
+    const generateInput = document.getElementById('generateInputBox').value;
+    const submitInput = document.getElementById('submitInputBox').value;
+    if(generateInput === submitInput){
+    const submitDone = document.getElementById('pin-matched');
         submitDone.style.display = 'block';
-        document.getElementById("generateInputBox").value = ' ';
-        document.getElementById("submitInputBox").value = ' ';
+        document.getElementById('generateInputBox').value = ' ';
+        document.getElementById('submitInputBox').value = ' ';
     }
-    if( generateInput !== submitInput  ){
-        const submitError = document.getElementById("errorMessage");
+    
+    if(generateInput !== submitInput){
+    const submitError = document.getElementById('errorMessage');
         submitError.style.display = 'block';
-        const countTry = document.getElementById("countThreeTimes");
-        let decreaseCount = parseInt(countTry.innerText) -1;
-        countTry.innerText = decreaseCount;
-        if( countTry.innerText == 3){
-            if(generateInput == submitInput.value){
-                countTry.innerText = 3 + 'try left';
+
+        const tryLeftCount = document.getElementById("tryAgainCount");
+        let decrease = parseInt(tryLeftCount.innerText)-1;
+        tryLeftCount.innerText = decrease;
+        if(tryLeftCount.innerText == 3){
+            if(generateInput.value == submitInput.value){
+                tryLeftCount.innerText= 3 + ' try left';
             }
         }
-        if( countTry.innerText == 2){
-            if(generateInput == submitInput.value){
-                countTry.innerText = 2 + 'try left';
+        if(tryLeftCount.innerText == 2){
+            if(generateInput.value == submitInput.value){
+                tryLeftCount.innerText= 2 + ' try left';
             }
         }
-        if( countTry.innerText == 1){
-            if(generateInput == submitInput.value){
-                countTry.innerText = 1 + 'try left';
+        if(tryLeftCount.innerText == 1){
+            if(generateInput.value == submitInput.value){
+                tryLeftCount.innerText= 1 + ' try left';
             }
         }
-        if(countTry.innerText <= 0){
-            if(generateInput == submitInput.value){
-                alert("Too many time tried. Please check your pin again")
-                countTry.innerText = 0 + 'try left';
+        if(tryLeftCount.innerText <= 0){
+            if(generateInput.value == submitInput.value){
+                alert('You enter the wrong pin. Please re-check it again ')
+                tryLeftCount.innerText= 0 + ' try left';
             }
-            document.getElementById("generateInputBox").value = ' ';
-            document.getElementById("submitInputBox").value = ' ';
+            document.getElementById('generateInputBox').value = ' ';
+            document.getElementById('submitInputBox').value = ' ';            
         }
     }
+
 });
